ce23439164e42a6494b2e025fd01663c1964f2fa
jibblyjobbly <jibblyjobbly@users.noreply.github.com>
2025-07-23 08:28:33 +0000

    Renaming with your datas

--- NightmareMode/Loader.cs
+++ NightmareMode/Loader.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace WinchModTemplate
+namespace NightmareMode
 {
 	public class Loader
 	{
@@ -9,8 +9,8 @@ namespace WinchModTemplate
 		/// </summary>
 		public static void Initialize()
 		{
-			var gameObject = new GameObject(nameof(WinchModTemplate));
-			gameObject.AddComponent<WinchModTemplate>();
+			var gameObject = new GameObject(nameof(NightmareMode));
+			gameObject.AddComponent<NightmareMode>();
 			GameObject.DontDestroyOnLoad(gameObject);
 		}
 	}
---
+++ NightmareMode/NightmareMode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace NightmareMode
+{
+	public class NightmareMode : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(NightmareMode)} has loaded!");
+		}
+	}
+}
